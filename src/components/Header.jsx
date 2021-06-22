68e76dfb8f5b331a87b3d62502cc250c8c44b243
alfredosumosav <asumosa1993@gmail.com>
2021-06-21 20:49:37 -0500

    implement theme context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import "./Header.css"
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const color = useContext(ThemeContext);
 
   const handleClick = () => {
     setDarkMode(!darkMode);
@@ -12,7 +14,7 @@ const Header = () => {
 
   return (
     <div className="Header">
-      <h1>ReactHooks</h1>
+      <h1 style={{ color }}>ReactHooks</h1>
       <label id="drkmod-btn" className="btn light" onClick={handleClick}>
         {darkMode ? 'Light Mode' : 'Dark Mode'}
       </label>
