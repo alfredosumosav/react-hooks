c026c465f18d7c1ee1a201a3fe5a288325b1f780
alfredosumosav <asumosa1993@gmail.com>
2021-07-02 10:45:39 -0500

    remove react component optimization example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from './components/Header';
 import Characters from './components/Characters';
-import Parent from "./components/Parent";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Parent />
       <Header />
       <Characters />
     </div>
