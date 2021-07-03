25455093c886a68c63ff1f6962ce1fd73c80b05b
alfredosumosav <asumosa1993@gmail.com>
2021-07-03 13:05:33 -0500

    fix cors permissions

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -3,7 +3,14 @@ import { useState, useEffect } from "react";
 const useCharacters = url => {
   const [characters, setCharacters] = useState([]);
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+     })
       .then(response => response.json())
       .then(data => setCharacters(data.results));
   }, [url]);
